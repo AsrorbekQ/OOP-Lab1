e645f45cad4d34bdb17910aaf650f89c8d73a4b0
Asrorbek Qalandarov <70290090+AsrorbekQ@users.noreply.github.com>
2025-08-27 15:08:51 +0500

    Implement digit sum calculation function

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -1,8 +1,24 @@
+//
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int humain(int argc, const char * argv[]) {
+
+    int inpt;
+    cout << "Enter the number to calculate sum of the digits: " << endl;
+    cin >> inpt;
+    
+    int sum = 0;
+    
+    while (inpt != 0) {
+        sum = sum + (inpt % 10);
+        inpt /= 10;
+    }
+    
+    cout << "Sum of digits: " << sum << endl;
+    
     return 0;
+    
 }
